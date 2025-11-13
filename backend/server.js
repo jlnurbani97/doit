1cbd377b2d4acae15923dee0fab5e40a5d122043
JulenUrbani <julen.urbani@studio.unibo.it>
2025-11-13 20:31:55 +0100

    SCRUM-37: server.js creation

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+//Server config page
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
