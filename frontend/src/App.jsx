b02f214a1197e7314a1b28828e7bbf180846f8cd
JulenUrbani <julen.urbani@studio.unibo.it>
2025-11-29 17:50:16 +0100

    SCRUM-59: Inizializzazione frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <div className="bg-red-500 text-white p-4">Hello world!</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
